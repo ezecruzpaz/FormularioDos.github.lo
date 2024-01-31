c15722d8c079c2ef5a04d6a51d29aa0e01878967
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-01-30 20:21:49 -0600

    Update service-worker.js

--- Formulario/service-worker.js
+++ Formulario/service-worker.js
@@ -1,18 +1,21 @@
-// service-worker.js
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-          return cache.addAll([
-              // Agrega aquí los recursos que deseas cachear
-          ]);
-      })
-  );
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'styles.css', // Agrega la ruta correcta a tu archivo CSS
+                'js/forumulario.js', // Agrega la ruta correcta a tu archivo JavaScript
+                // Agrega aquí los recursos que deseas cachear
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-      caches.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
