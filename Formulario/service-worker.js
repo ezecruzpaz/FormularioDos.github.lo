51673ed4efd36acdb62a4be14ecc055304740e9f
ezecruzpaz <144050109+ezecruzpaz@users.noreply.github.com>
2024-01-30 20:29:12 -0600

    Update service-worker.js

--- Formulario/service-worker.js
+++ Formulario/service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
             return cache.addAll([
                 '/',
                 'index.html',
-                'styles.css', // Agrega la ruta correcta a tu archivo CSS
+                'css/estilo.css', // Agrega la ruta correcta a tu archivo CSS
                 'js/forumulario.js', // Agrega la ruta correcta a tu archivo JavaScript
                 // Agrega aquí los recursos que deseas cachear
             ]);
